618ffe7d0ba21ce4f682c1dd57dcd71b783b6ef9
Brett <58530824+Bearium-BB@users.noreply.github.com>
2023-05-03 19:51:54 -0500

    just a save

---
+++ How2Game.DataAccess/User/UserCRUD.cs
@@ -0,0 +1,25 @@
+﻿using How2Game.DataAccess.Data;
+using How2Games.Domain.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace How2Game.DataAccess.User
+{
+    internal class UserCRUD
+    {
+        private GamesContext context;
+
+        public void Insert() { 
+            How2GamesUser user = new How2GamesUser();
+            user.UserName = "name";
+            user.Password = "password";
+            user.Email = "email";
+            user.FullName = "name2";
+            context.Add(user);
+            context.SaveChanges();
+        }
+    }
+}
