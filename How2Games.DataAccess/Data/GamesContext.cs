7d2d26f105ff33a0e6c3d24a50f0d8f654112968
Brett <58530824+Bearium-BB@users.noreply.github.com>
2023-05-31 10:38:29 -0500

    save

--- How2Games.DataAccess/Data/GamesContext.cs
+++ How2Games.DataAccess/Data/GamesContext.cs
@@ -16,13 +16,6 @@ namespace How2Games.DataAccess.Data
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Image> Images { get; set; }
         public DbSet<Post> Posts { get; set; }
-        public DbSet<PostComment> PostComments { get; set; }
-        public DbSet<PostImage> PostImages { get; set; }
-        public DbSet<PostText> PostTexts { get; set; }
-        public DbSet<Question> Questions { get; set; }
-        public DbSet<QuestionComment> QuestionComments { get; set; }
-        public DbSet<QuestionImage> QuestionImages { get; set; }
-        public DbSet<QuestionText> QuestionTexts { get; set; }
         public DbSet<TextBox> Texts { get; set; }
         public DbSet<How2GamesUser> Users { get; set; }
         public DbSet<Game> Games { get; set; }
--- How2Games.Domain/DB/Post.cs
+++ How2Games.Domain/DB/Post.cs
@@ -5,5 +5,11 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public int UserId { get; set; }
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public ICollection<Image> Images { get; set; } = new List<Image>();
+        public ICollection<TextBox> TextBoxes { get; set; } = new List<TextBox>();
+
+
+
     }
 }
