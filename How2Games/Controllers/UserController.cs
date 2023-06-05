de2863ea9f7bfddfb0e68a2fe648f8945cafc8cd
Zack-Z23 <zack.zacharidis@gmail.com>
2023-06-05 09:14:15 -0500

    Merge conflct

--- How2Games/Controllers/UserController.cs
+++ How2Games/Controllers/UserController.cs
@@ -37,28 +37,37 @@ namespace How2Games.Controllers
             return View();
         }
 
-
+        //Sign up 
+       
         [HttpPost]
+        //Prevent cross-site attacks 
         [ValidateAntiForgeryToken]
 
         public async Task<IActionResult> SignUp(FormUser user)
         {
+            //Grabs user's first name and last 
             user.FullName = user.FirstName + " " + user.LastName;
+            //Inserts the new user into the database
             await _userCRUDServices.Insert(user.FullName, user.Email, user.UserName, user.Password);
+            //Grabs the inserted user's information and then signs them in immidiately after registering
             var result = await _signInManager.PasswordSignInAsync(_gamesContext.Users.FirstOrDefault(x => x.UserName == user.UserName), user.Password, true, false);
+            
             if (result.Succeeded)
             {
-
+                //If the sign in succeeds, it will return them to the home page
                 return RedirectToAction("Index", "Home");
             }
             else
             {
+                //It will throw a new argument expeciton
                 throw new ArgumentException("Invalid Sign Up attempt");
 
             }
 
         }
+        //Login
         [HttpPost]
+        //Prevent cross-site attacks 
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> LogIn(FormUser user)
         {
@@ -75,15 +84,16 @@ namespace How2Games.Controllers
             //    new ServiceCollection().BuildServiceProvider(),
             //    NullLogger<UserManager<How2GamesUser>>.Instance
 
-
+            //Grabs the user's username and then returns the user model
             var testUser = _gamesContext.Users.FirstOrDefault(x => x.UserName == user.UserName);
             if (testUser != null)
             {
+                //If the user is found then this will grab the user's password and make sure its the same as the user model from the server
                 var result = await _signInManager.CheckPasswordSignInAsync(testUser, user.Password, false);
 
                 if (result.Succeeded)
                 {
-
+                    //Finally if the result succeeds then it will sign them in and return them to homepage
                     await _signInManager.SignInAsync(testUser, isPersistent: false);
                     return RedirectToAction("Index", "Home");
                 }
@@ -100,8 +110,10 @@ namespace How2Games.Controllers
 
 
         }
+        //Log out
         public IActionResult LogOut()
         {
+            //It signs the user out and redirects them back to home page
             _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
