d9f13b19e601f4f7c56fffc4360ff961417e6966
Bearium-BB <brett200410@gmail.com>
2023-06-12 23:19:17 -0500

    god plz

---
+++ How2Games.DomainTests3/HelperFunctions/DeveloperTagHelperFunctionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using How2Games.Domain.HelperFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using How2Games.Domain.DB;
+
+namespace How2Games.Domain.HelperFunctions.Tests
+{
+    [TestClass()]
+    public class DeveloperTagHelperFunctionsTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+
+            // Arrange
+            string text = null;
+            DeveloperTag dt = new DeveloperTag();
+
+            dt.Text = text;
+           
+            // Assert
+            Assert.IsNull(dt.Text);
+
+        }
+    }
+}
