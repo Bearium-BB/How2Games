f4935ca8f3790d9a1c542a18d33f921e8f88546e
abtin <abtinravesh@gmail.com>
2023-06-12 23:14:48 -0500

    added injection

---
+++ How2Games.DataAccess/QuestionAction/AdminMenuQuestion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using How2Games.DataAccess.Data;
+using How2Games.DataAccess.TagAction;
+using How2Games.Domain.DB;
+using Newtonsoft.Json;
+
+namespace How2Games.DataAccess.QuestionAction
+{
+    public class AdminMenuQuestion
+    {
+        
+    private readonly GamesContext _gamesContext;
+        public AdminMenuQuestion(GamesContext context)
+        {
+            _gamesContext = context;
+        }
+        public void DeleteQuuestion(int questionId)
+        {
+            var question = _gamesContext.Questions.FirstOrDefault(x=> x.Id == questionId);
+            if (question != null)
+            {
+
+                _gamesContext.Questions.Remove(question);
+
+            }
+            
+        }
+    }
+}
