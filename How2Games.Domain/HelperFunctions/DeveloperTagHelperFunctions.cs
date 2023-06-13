31b2496d99de4afbcdbe5d4b0d7f9cff782b4ed8
abtin <abtinravesh@gmail.com>
2023-06-12 21:58:51 -0500

    finsihed unit test

---
+++ How2Games.Domain/HelperFunctions/DeveloperTagHelperFunctions.cs
@@ -0,0 +1,20 @@
+﻿using How2Games.Domain.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace How2Games.Domain.HelperFunctions
+{
+    internal class DeveloperTagHelperFunctions
+    {
+        public DeveloperTag Create(string text)
+        {
+
+            DeveloperTag NewTag = new DeveloperTag();
+            NewTag.Text = text;
+            return NewTag;
+        }
+    }
+}
